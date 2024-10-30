e7949846e58c3b5b4e4216af0fb7074e8f88f2bc
Guy Abel <g.j.abel@gmail.com>
2024-10-30 12:35:26 +0800

    wpp2024 update
     + add oricid and bluesky

--- js/wowchemy-slides.js
+++ js/wowchemy-slides.js
@@ -2,7 +2,7 @@
   // ns-params:@params
   var slides = {highlight_style: "dracula", theme: "black"};
 
-  // ns-hugo:C:\Users\Guy\Documents\GitHub\personal-site\themes\github.com\wowchemy\wowchemy-hugo-modules\wowchemy\assets\js\wowchemy-utils.js
+  // ns-hugo:C:\Users\Guy Abel\Documents\Github\personal-site\themes\github.com\wowchemy\wowchemy-hugo-modules\wowchemy\assets\js\wowchemy-utils.js
   function fixMermaid(render = false) {
     let mermaids = [];
     [].push.apply(mermaids, document.getElementsByClassName("language-mermaid"));
